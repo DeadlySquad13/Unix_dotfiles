70da75fdcbc1ab1f3c5596bf16383084a117d011
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-11-17 17:05:40 +0000

    [Enh]: Added cdpath.

--- home.nix
+++ home.nix
@@ -96,7 +96,7 @@
   #
   # if you don't want to manage your shell through Home Manager.
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    # CDPATH = "~/.bookmarks:/mnt/e";
   };
 
   programs = {

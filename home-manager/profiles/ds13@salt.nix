3459fa938a6624c0ca3e6c6569931d6e86dc2b02
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-04-21 18:04:42 +0000

    [Refactor](ds13@salt): Moved home username and directory vars to profile.

--- home-manager/profiles/ds13@salt.nix
+++ home-manager/profiles/ds13@salt.nix
@@ -1,7 +1,19 @@
 { config, lib, ... }:
 
 {
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
   home.stateVersion = "23.05";
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "ds13";
+  home.homeDirectory = "/home/ds13";
+
   imports =  
   [ 
     ../apps/ripgrep.nix

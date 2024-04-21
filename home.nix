9405e6c6461ebd6e62561bcdd97f1259f55d26e2
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-04-21 17:38:29 +0000

    [Enh](Bookmarks): Added proper nesting.

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   nix = {
@@ -97,15 +97,38 @@
   } // # Bookmarks.
   builtins.mapAttrs
       (name: value: { source = config.lib.file.mkOutOfStoreSymlink value; })
-      {
-        ".bookmarks/config" = ~/.config;
-        ".bookmarks/shared_configs" = "/shared/archive-resources-/Shared/Configs";
-        ".bookmarks/shared_scripts" = "/shared/archive-resources-/Shared/_scripts";
-        ".bookmarks/kbd" = "/shared/archive-resources-/Resources/KnowledgeBase__Data";
+      (
+        lib.attrsets.concatMapAttrs (name: value: { ".bookmarks/${name}" = value; } )
+        (
+          {
+            "config" = ~/.config;
+            "projects" = ~/Projects;
+          } //
+          # Linking to shared `archive-resources-`.
+          builtins.mapAttrs (name: value: "/shared/archive-resources-/${value}") {
+            "shared-configs" = "Shared/Configs";
+            "shared-scripts" = "Shared/_scripts";
 
-        # More specific.
-        ".bookmarks/qmk" = "/shared/archive-resources-/Shared/Configs/Keyboard__/ErgohavenVialQmk/keyboards/ergohaven/k02/keymaps/DeadlySquad13";
-      };
+            # ?: Should be in Shared too?
+            "kbd" = "Resources/KnowledgeBase__Data";
+
+            # More specific.
+            "qmk" = "Shared/Configs/Keyboard__/ErgohavenVialQmk/keyboards/ergohaven/k02/keymaps/DeadlySquad13";
+          } //
+          # # Projects
+          builtins.mapAttrs (name: value: "/home/ds13/Projects/${value}") {
+            "mlops" = "--educational/MlOps_course__Tasks";
+          } //
+          # # Shared Projects
+          builtins.mapAttrs (name: value: "/shared/soft-projects-/ds13/Projects/${value}") {
+            "shared-projects" = "";
+            "Programming_dotfiles.bootstrap" = "--personal/Programming_dotfiles.bootstrap";
+          }
+        ) //
+        {
+          "Projects/shared-" = /shared/soft-projects-/ds13/Projects;
+        }
+      );
 
   # You can also manage environment variables but you will have to manually
   # source

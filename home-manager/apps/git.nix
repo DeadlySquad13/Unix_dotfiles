7e59e7b60f575e3539fa37fbac9dd975f2d4b493
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-06-18 13:19:36 +0300

    [Enh](Git): Added global_gitignore.

--- home-manager/apps/git.nix
+++ home-manager/apps/git.nix
@@ -3,6 +3,7 @@
 {
   home.file = {
     ".gitconfig".source = ~/.bookmarks/shared-configs/Wsl2_dotfiles/stow_home/git/.gitconfig;
+    ".config/git/.global_gitignore".source = ~/.bookmarks/shared-configs/Wsl2_dotfiles/stow_home/git/.global_gitignore;
   };
 
   home.packages = with pkgs; [

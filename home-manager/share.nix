69e0f847fb64696783982c1d1f6f7f37aad2bf18
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-06-19 20:35:21 +0300

    [Enh](Share): Added links.

--- home-manager/share.nix
+++ home-manager/share.nix
@@ -8,10 +8,21 @@
       (
         builtins.mapAttrs (name: value: "/Users/aspakalo/.local/share-/${value}")
         {
+          # Mapping .local/share/ dirs = to my local share
           "bash/history" = "Bash_history";
           "common-list-jupyter" = "Maxima_history";
           "mind.nvim" = "MindNvim";
           "nix/repl-history" = "Nix__Repl_history";
+          "ranger" = "Ranger";
+          "wezterm/repl-history" = "Wezterm__Repl_history";
+
+          "nvim/auto_session" = "Nvim/auto_session";
+          "nvim/project_nvim" = "Nvim/project_nvim";
+          "nvim/sessions" = "Nvim/sessions";
+          "nvim/spell" = "Nvim/spell";
+          "nvim/view" = "Nvim/view";
+          "nvim/undo" = "Nvim/undo";
+          "nvim/telescope_history" = "Nvim/telescope_history";
         }
       )
     );

c50d7f787b764f89504eabbf80137acea9229cb8
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-06-22 12:55:04 +0000

    [Enh](Uair): Added yad and moved config out of store.

--- home-manager/apps/uair.nix
+++ home-manager/apps/uair.nix
@@ -1,8 +1,17 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
+  home.file = {
+    ".config/uair" = {
+      source = config.lib.file.mkOutOfStoreSymlink ~/.bookmarks/home-configs/uair;
+      recursive = true;
+    };
+  };
+
   home.packages = with pkgs; [
     # Extensible pomodoro timer.
     uair
+    # Gui dialog for shell scripts. Used for displaying uair stdout timer.
+    yad
   ];
 }

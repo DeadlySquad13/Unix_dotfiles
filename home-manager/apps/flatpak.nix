fa1331d09e3f17ed68c53a4de6423db3c4de5aa4
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-13 11:48:06 +0000

    [Fix](Flatpak): Removed xdg_data_dirs because it was breaking bash.

--- home-manager/apps/flatpak.nix
+++ home-manager/apps/flatpak.nix
@@ -4,7 +4,7 @@
   programs.bash = {
     sessionVariables = {
       # For flatpak
-      XDG_DATA_DIRS = "/var/lib/flatpak/exports/share:/home/ds13/.local/share/flatpak/exports/share";
+      # XDG_DATA_DIRS = "/var/lib/flatpak/exports/share:/home/ds13/.local/share/flatpak/exports/share";
     };
   };
 }

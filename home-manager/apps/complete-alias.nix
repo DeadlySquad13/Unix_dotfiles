a786bfe7b9f1d7b2eb22bc5645ae8c25de48cd11
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-07-23 00:21:03 +0000

    [Enh](Apps): Added complete-alias.

--- home-manager/apps/complete-alias.nix
+++ home-manager/apps/complete-alias.nix
@@ -4,4 +4,12 @@
   home.packages = with pkgs; [
     complete-alias
   ];
+
+  programs.bash = {
+    # TODO: Get this path without hardcode.
+    # I got it from `which complete_alias`
+    profileExtra = ''
+      [[ -f $HOME/.nix-profile/bin/complete_alias ]] && . /home/ds13/.nix-profile/bin/complete_alias
+    '';
+  };
 }

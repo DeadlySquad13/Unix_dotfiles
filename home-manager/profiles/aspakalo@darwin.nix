530b15cf5b52d7c2ee1bbc4091cd3750630366ad
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-13 14:47:21 +0000

    [Refactor]: Cleaned profiles from things that are already moved to snowfall.

--- home-manager/profiles/aspakalo@darwin.nix
+++ home-manager/profiles/aspakalo@darwin.nix
@@ -1,20 +1,7 @@
 { pkgs, config, lib, ... }:
 
+# Everything else is already moved.
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "aspakalo";
-  home.homeDirectory = "/Users/aspakalo";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11";
-
   imports =  
   [ 
     ../share.nix
@@ -42,40 +29,6 @@
 
     # ../apps/mattermost.nix
   ];
-
-  home.file = {
-    # TODO: Move from stow to the structure similar to ds13@salt bookmarks.
-    ".bash".source = lib.mkForce ~/dotfiles/stow_home/bash/.bash;
-  } // # Bookmarks.
-  builtins.mapAttrs
-      (name: value: { source = config.lib.file.mkOutOfStoreSymlink value; })
-      (
-        lib.attrsets.concatMapAttrs (name: value: { ".bookmarks/${name}" = value; } )
-        ({
-          "config" = ~/.config;
-          "kbd" = ~/KnowledgeBase__Data;
-          "projects" = "/Users/aspakalo/Projects";
-          "shared-scripts" = ~/shared-scripts;
-          # "shared-configs" = ~/shared-configs;
-        } //
-        # More specific.
-        # # Projects
-        builtins.mapAttrs (name: value: "/Users/aspakalo/Projects/${value}") {
-          # Namespaces.
-          "ephemeral-projects" = "ephemeral-";
-          "interim-projects" = "interim-";
-
-          # Specific projects.
-          "lp" = "logistic-platform";
-          "shepherd" = "shepherd";
-          "kit" = "main-ui-kit";
-          "@kit" = "main-ui-kit/packages";
-          "@components" = "main-ui-kit/packages/components";
-          "kit2" = "main-ui-kit2";
-          "@kit2" = "main-ui-kit2/packages";
-          "@components2" = "main-ui-kit2/packages/components";
-        })
-      );
 
   programs = {
     # FIX: Nix wasn't enabled until sourced it manually.

de544158431e772fdbd2c83337ea6044f05325eb
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-13 18:02:54 +0000

    [Enh](KrokiCli): Added config.

--- modules/home/apps/architecturing/kroki-cli/default.nix
+++ modules/home/apps/architecturing/kroki-cli/default.nix
@@ -1,7 +1,24 @@
-{ pkgs, ... }:
-
-{
+{pkgs, config, ...}: let
+  kroki-config-path = "${config.home.homeDirectory}/.config/kroki/kroki.yml";
+in {
   home.packages = with pkgs; [
     ds-omega.kroki-cli
   ];
+
+  programs.bash = {
+    shellAliases = {
+      # Diagram Create
+      dc = "kroki";
+    };
+    sessionVariables = {
+      KROKI_CONFIG = kroki-config-path;
+    };
+  };
+
+  home.file = {
+    ${kroki-config-path}.text = ''
+      endpoint: 'http://localhost:8010'
+      timeout: 5s
+    '';
+  };
 }

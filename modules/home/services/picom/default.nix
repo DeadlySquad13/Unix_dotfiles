5c8e8fef8d08db9529e0cd7862d51e32ce2ba9b7
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-16 11:17:51 +0000

    [Enh]: Init new group of modules - services. Added picom to it.

---
+++ modules/home/services/picom/default.nix
@@ -0,0 +1,19 @@
+{...}: {
+  services.picom = {
+    enable = true;
+    # Tried to fix flickering
+    # [Nvidia NixOs wiki][@NvidiaNixOSWiki]|[Zotero][z@NvidiaNixOSWiki].
+    # [Picom Arch wiki][@PicomArchWiki]|[Zotero][z@PicomArchWiki]
+    vSync = true;
+    settings = {
+      unredir-if-possible = false;
+    };
+  };
+}
+
+/* References:
+[@NvidiaNixOSWiki]: <https://nixos.wiki/wiki/Nvidia#Using_GPUs_on_non-NixOS> 'Nvidia - NixOS Wiki'
+[z@NvidiaNixOSWiki]: <zotero://select/items/@NvidiaNixOSWiki> 'Select in Zotero: Nvidia - NixOS Wiki'
+[@PicomArchWiki]: <https://wiki.archlinux.org/title/Picom#Configuration> 'Picom - ArchWiki'
+[z@PicomArchWiki]: <zotero://select/items/@PicomArchWiki> 'Select in Zotero: Picom - ArchWiki'
+*/

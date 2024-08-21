55923a65ef56927f4ff0e54133a9783fcb93fcfe
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-21 14:40:01 +0300

    [Enh]: Added prod fetch for wezterm.

--- modules/home/apps/general/wezterm/default.nix
+++ modules/home/apps/general/wezterm/default.nix
@@ -4,11 +4,17 @@
   home.file = {
     # # xdg home should be set to "$HOME/.config", otherwise wezterm will look
     # in another location.
-    ".config/wezterm".source = ~/.bookmarks/shared-configs/WezTerm_config;
+    # ".config/wezterm".source = ~/.bookmarks/shared-configs/WezTerm_config;
+    ".config/wezterm".source = pkgs.fetchFromGitHub {
+  owner = "DeadlySquad13";
+  repo = "WezTerm_config";
+  rev = "a62f98f6451fd678b7454a3c91cb6a27a9952915";
+  hash = "sha256-BBuG5ukBp0pCkpENttevE1q2DSXy+Pg5qazbiVlQBOQ=";
+};
   };
 
   home.packages = with pkgs; [
     # Had some problems with package from nixpkgs in 2023.
-    # wezterm
+    wezterm
   ];
 }

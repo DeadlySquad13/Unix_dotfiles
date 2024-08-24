039ed92c411483c30f2a67cd32cf2afa08c80ba5
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-24 16:37:45 +0000

    [Fix](Rofi): Temporarily removed mkIfEnabled because it was causing strange resurcion errors.

--- modules/home/apps/gui-utility/rofi/default.nix
+++ modules/home/apps/gui-utility/rofi/default.nix
@@ -1,5 +1,16 @@
-{ ... }:
-
+# FIX: For some reason not working only here...
+# {
+#   lib,
+#   namespace,
+#   config,
+#   ...
+# }:
+# lib.${namespace}.mkIfEnabled {
+#   inherit config;
+#   category = "gui-utility";
+#   name = "rofi";
+# }
+{...}:
 {
   programs.rofi = {
     enable = true;

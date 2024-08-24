6008fd58ba4099a452be871ddfe1f8622bfe3846
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-24 11:57:29 +0000

    [Style](Ranger): Changed from */dev- to */-dev to make it similar to tags.

--- modules/home/apps/cli-utility/ranger/dev.nix
+++ modules/home/apps/cli-utility/ranger/dev.nix
@@ -12,13 +12,13 @@ in
   mkIf (cfg.enabled && cfg.dev) {
     home.file = {
       # Dev.
-      ".local/dotfiles-/_configs/ranger/dev-/commands.py" = {
+      ".local/dotfiles-/_configs/ranger/-dev/commands.py" = {
         source = config.lib.file.mkOutOfStoreSymlink ~/.bookmarks/shared-configs/Wsl2_dotfiles/stow_home/ranger/.config/ranger/commands.py;
       };
     };
 
     home.shellAliases = {
-      ranger-dev = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/dev-";
-      r-dev = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/dev-";
+      ranger-dev = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/-dev";
+      r-dev = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/-dev";
     };
   }
--- modules/home/apps/cli-utility/ranger/stage.nix
+++ modules/home/apps/cli-utility/ranger/stage.nix
@@ -13,11 +13,11 @@ in
     && cfg.stage) {
     home.file = {
       # Stage.
-      ".local/dotfiles-/_configs/ranger/stage-/commands.py".source = ~/.bookmarks/shared-configs/Wsl2_dotfiles/stow_home/ranger/.config/ranger/commands.py;
+      ".local/dotfiles-/_configs/ranger/-stage/commands.py".source = ~/.bookmarks/shared-configs/Wsl2_dotfiles/stow_home/ranger/.config/ranger/commands.py;
     };
 
     home.shellAliases = {
-      ranger-stage = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/stage-";
-      r-stage = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/stage-";
+      ranger-stage = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/-stage";
+      r-stage = "ranger --confdir=~/.local/dotfiles-/_configs/ranger/-stage";
     };
   }

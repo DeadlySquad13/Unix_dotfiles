04af8d2d27835c5f1abc0f0a27ad034b7a300e99
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-08-24 11:56:45 +0000

    [Fix](NeoVim): Moved to nvim_appname to create 3-way staging.

--- modules/home/apps/general/neovim/dev.nix
+++ modules/home/apps/general/neovim/dev.nix
@@ -12,18 +12,20 @@ in
   mkIf (cfg.enabled && cfg.dev) {
     home.file = {
       # Reference: https://www.reddit.com/r/NixOS/comments/104l0w9/comment/jhfxdq4/?utm_source=share&utm_medium=web2x&context=3
+
+      # Linked it here just for uniformity. Didn't find a way to point
+      # NVIM_APPNAME to it.
       ".local/dotfiles-/_configs/nvim/-dev" = {
         source = config.lib.file.mkOutOfStoreSymlink /shared/archive-resources-/Shared/_configs/NeoVim_config;
         recursive = true;
       };
-      # Use NVIM_APPNAME
       ".config/nvim-dev" = {
         source = config.lib.file.mkOutOfStoreSymlink /shared/archive-resources-/Shared/_configs/NeoVim_config;
         recursive = true;
       };
     };
     home.shellAliases = {
-      nvim-dev = "nvim -u ~/.local/dotfiles-/_configs/nvim/dev-/init.vim";
-      vi-dev = "nvim -u ~/.local/dotfiles-/_configs/nvim/dev-/init.vim";
+      nvim-dev = "NVIM_APPNAME=nvim-dev nvim";
+      vi-dev = "NVIM_APPNAME=nvim-dev nvim";
     };
   }

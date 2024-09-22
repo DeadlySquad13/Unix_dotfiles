42f7820db5cc428718d5d1cd2fb0b259f2e1982a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-09-22 18:49:32 +0300

    [Enh](Broot): Added verbs for vi-dev and vi-stage.

--- modules/home/apps/cli-utility/broot/default.nix
+++ modules/home/apps/cli-utility/broot/default.nix
@@ -17,6 +17,20 @@ lib.${namespace}.mkIfEnabled {
       verbs = [
         { invocation = "r"; execution = "ranger {directory}"; leave_broot = false; }
         { invocation = "ranger"; execution = "ranger {directory}"; leave_broot = false; }
+
+        # TODO: Move vi-dev aand vi-stage to bash utils and set `leave_broot
+        # = false` and remove `from_shell`.
+        #
+        # `from_shell = true` and `leave_broot = false` are not compatible.
+        # I wish I had `leave_broot = false```, though. But broot doesn't interpret
+        # aliases (it seems it doesn't source bashrc) and doesn't work with `NVIM_APPNAME=nvim-dev nvim`
+        # (it says "No such file or directory" - I assume it interprets
+        # variable set incorrectly).
+        { invocation = "es"; execution = "vi-stage {file}"; from_shell = true; leave_broot = true; }
+        { invocation = "edit-stage"; execution = "vi-stage {file}"; from_shell = true; leave_broot = true; }
+
+        { invocation = "edit-dev"; execution = "vi-dev {file}"; from_shell = true; leave_broot = true; }
+        { invocation = "ed"; execution = "vi-dev {file}"; from_shell = true; leave_broot = true; }
       ];
     };
   };

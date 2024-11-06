52f6ac96c6d3de25043aef209acafc4a7c683fc6
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-11-06 18:14:32 +0300

    [Enh](Ecosystem): Init sops.

---
+++ modules/home/apps/ecosystem/sops/default.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  namespace,
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+} // lib.${namespace}.mkIfEnabled {
+  inherit config;
+  category = "ecosystem";
+  name = "sops";
+}
+{
+  home.packages = with pkgs; [
+    # Used for generating age file `age-keygen`.
+    age
+  ];
+
+  sops = {
+    age.keyFile = "/Users/apakalo/.config/sops/age/keys.txt"; # must have no password!
+    # REFACTOR: Targeting root of Unix_dotfiles.
+    defaultSopsFile = ../../../../../secrets/secrets.yaml;
+    secrets.glab_DeadlySquad13_token = {
+    #   sopsFile = ../../../../../secrets/secrets.yaml; # optionally define per-secret files
+
+    #   # %r gets replaced with a runtime directory, use %% to specify a '%'
+    #   # sign. Runtime dir is $XDG_RUNTIME_DIR on linux and $(getconf
+    #   # DARWIN_USER_TEMP_DIR) on darwin.
+      # path = "%r/test.txt"; 
+    };
+  };
+}

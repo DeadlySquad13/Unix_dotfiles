159a7823f44fb86e7a6361a6c2ea2af44f5abb4c
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-01-04 10:08:01 +0000

    [Enh]: Init olivier system.

---
+++ homes/x86_64-linux/ds13@olivier/default.nix
@@ -0,0 +1,86 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  # Additional metadata is provided by Snowfall Lib.
+  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+  home, # The home architecture for this host (eg. `x86_64-linux`).
+  target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
+  format, # A normalized name for the home target (eg. `home`).
+  virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
+  host, # The host name for this home.
+  # All other arguments come from the home home.
+  config,
+  ...
+}:
+let
+  inherit (lib.${namespace}) disabled enabled;
+  in
+{
+  home = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.11";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "ds13";
+    homeDirectory = "/home/ds13";
+  };
+
+  lib.${namespace}.modules = {
+    architecturing = {
+      enable = true;
+    };
+    cli-utility = {
+      enable = true;
+    };
+    development = {
+      enable = true;
+    };
+    ecosystem = {
+      enable = true;
+    };
+    general = {
+      enable = true;
+
+      neovim = {
+        enabled = true; # TODO: Remove.
+        dev = true;
+        stage = true;
+      };
+
+      openvpn3 = disabled;
+    };
+    gui-utility = {
+      enable = true;
+    };
+    productivity = {
+      enable = true;
+    };
+    misc = {
+      qmk = enabled;
+    };
+    writing = {
+      enable = true;
+    };
+
+    tools = {
+      enabled = true;
+
+      ranger = {
+        enabled = true;
+        dev = true;
+        stage = true;
+      };
+    };
+  };
+}
---
+++ systems/x86_64-linux/olivier/default.nix
@@ -0,0 +1,84 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  # Additional metadata is provided by Snowfall Lib.
+  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+  home, # The home architecture for this host (eg. `x86_64-linux`).
+  target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
+  format, # A normalized name for the home target (eg. `home`).
+  virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
+  host, # The host name for this home.
+  # All other arguments come from the home home.
+  config,
+  ...
+}: let
+  inherit (lib.${namespace}) disabled enabled;
+in {
+
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+  /*
+     snowfallorg.user = {
+    enable = true;
+    name = "ds-omega";
+  };
+  */
+
+  /*
+     home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+  */
+  system = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.11";
+  };
+
+  networking.hostName = "olivier";
+
+  # https://nix-community.github.io/NixOS-WSL/options.html
+  wsl = {
+    enable = true;
+    # Hard to set: https://discourse.nixos.org/t/set-default-user-in-wsl2-nixos-distro/38328
+    # defaultUser = "ds13";
+
+    docker-desktop.enable = true;
+  };
+
+  users.users.ds13 = {
+  isNormalUser  = true;
+  home  = "/home/ds13";
+  extraGroups  = [ "wheel" "networkmanager" ];
+  #initialPassword = "test";
+  password = "test";
+  };
+
+  # boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
+  services.sshd.enable = true;
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  lib.ds-omega = {
+    modules = {
+      ecosystem = {
+        enable = false;
+
+        nix = disabled;
+      };
+    };
+  };
+}

e0d9ac016bd25c0b90c6edd0dfe8c82763894438
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-01-04 10:11:45 +0000

    [Chore]: Temporarily disabled darwin specific packages.

--- modules/home/apps/general/skhd/default.nix
+++ modules/home/apps/general/skhd/default.nix
@@ -1,19 +1,3 @@
 { config, namespace, pkgs, lib, ... }:
-
-# ?: It is available only for macos. Make platform specific packages?
-let config-source = lib.${namespace}.source {
-        inherit config;
-        get-path = p: "${p.shared-configs}/Skhd_config";
-        out-of-store = true;
-      };
-in
 {
-  home.file = lib.mkIf (config-source != {}) {
-    # TODO: fetch from repo: https://github.com/DeadlySquad13/Skhd_config
-    ".config/skhd" = builtins.trace config-source config-source;
-  };
-
-  home.packages = with pkgs; [
-    skhd
-  ];
 }
--- modules/home/apps/general/yabai/default.nix
+++ modules/home/apps/general/yabai/default.nix
@@ -1,20 +1,3 @@
 { config, namespace, pkgs, lib, ... }:
-let 
-  config-source = lib.${namespace}.source {
-      inherit config;
-      get-path = p: "${p.home-configs}/YabaiWm_config";
-      out-of-store = false;
-    };
-in
-# ?: It is available only for macos. Make platform specific packages?
-# TODO: Add skhd as dep.
 {
-  home.file = lib.mkIf (config-source != {}) {
-    # TODO: fetch from repo: https://github.com/DeadlySquad13/YabaiWm_config
-    ".config/yabai" = config-source;
-  };
-
-  home.packages = with pkgs; [
-    yabai
-  ];
 }

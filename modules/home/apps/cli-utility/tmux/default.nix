126d5f1aab13330b68b5e1786dc3a66389acf530
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-04-05 14:02:24 +0000

    [Chore](Tmux): Added shortcut (last version wasn't working after update), changed to dev.

--- modules/home/apps/cli-utility/tmux/default.nix
+++ modules/home/apps/cli-utility/tmux/default.nix
@@ -19,18 +19,29 @@ lib.${namespace}.mkIfEnabled {
       tmuxPlugins.resurrect
     ];
 
+    # * Prefix remapping.
+    # (Actually <c-space>)
+    shortcut = "Space";
+
     extraConfig = "source ~/.config/tmux/old.conf";
   };
 
   # TODO: Configure properly via nix: https://nix-community.github.io/home-manager/options.xhtml#opt-programs.tmux.enable
   home.file = {
-    ".config/tmux/old.conf".source =
-        pkgs.fetchFromGitHub {
-          owner = "DeadlySquad13";
-          repo = "Wsl2_dotfiles";
-          rev = "af728d9f05f25656ab8fcefa66d767c5b558710e";
-          hash = "sha256-3hT3Gzh7vDRap1prJFyu+av4SS0kbu+HVYPbHRYw0YE=";
-        }
-        + "/stow_home/tmux/.tmux.conf";
+    # Dev.
+    ".config/tmux/old.conf" = lib.${namespace}.source {
+      inherit config;
+      get-path = p: "${p.shared-configs}/Wsl2_dotfiles/stow_home/tmux/.tmux.conf";
+      out-of-store = true;
+    };
+    # Prod.
+  #   ".config/tmux/old.conf".source =
+  #       pkgs.fetchFromGitHub {
+  #         owner = "DeadlySquad13";
+  #         repo = "Wsl2_dotfiles";
+  #         rev = "af728d9f05f25656ab8fcefa66d767c5b558710e";
+  #         hash = "sha256-3hT3Gzh7vDRap1prJFyu+av4SS0kbu+HVYPbHRYw0YE=";
+  #       }
+  #       + "/stow_home/tmux/.tmux.conf";
   };
 }

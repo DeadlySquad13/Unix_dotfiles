eb93e0dd9b3884ef15b51f28f5b359abe2160a64
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-04-05 14:00:27 +0000

    [Enh](CliUtility): Added shelloracle that integrates with llms.

---
+++ modules/home/apps/cli-utility/shelloracle/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  namespace,
+  config,
+  ...
+}:
+lib.${namespace}.mkIfEnabled {
+  inherit config;
+  category = "cli-utility";
+  name = "shelloracle";
+}
+{
+  programs.bash = {
+    bashrcExtra = ''
+      # Shelloracle.
+      [ -f ~/.shelloracle.bash ] && . ~/.shelloracle.bash
+    '';
+  };
+
+  home.file = {
+    ".shelloracle.bash".source = builtins.fetchurl {
+      # https://github.com/djcopley/ShellOracle/blob/16535e262ac7a3379bc5fa56426f225bb83da4ff/src/shelloracle/shell/shelloracle.bash
+      url = "https://raw.githubusercontent.com/djcopley/ShellOracle/16535e262ac7a3379bc5fa56426f225bb83da4ff/src/shelloracle/shell/shelloracle.bash";
+    };
+  };
+
+}

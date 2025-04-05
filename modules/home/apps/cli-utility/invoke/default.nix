357e19cb0bd08d8d22852602bf8e1cd2b8ce9a87
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-04-05 14:00:55 +0000

    [Enh](Invoke): Added config and auto-completion.

--- modules/home/apps/cli-utility/invoke/default.nix
+++ modules/home/apps/cli-utility/invoke/default.nix
@@ -15,9 +15,24 @@ lib.${namespace}.mkIfEnabled {
     python311Packages.invoke
   ];
 
+  home.file = {
+    ".invoke.yaml".text = ''
+      tasks:
+        search_root: ${lib.${namespace}.get-path { inherit config; cb = p: p.shared-scripts; }}
+    '';
+  };
+
   programs.bash = {
     shellAliases = {
-        i = "invoke --search-root ~/.bookmarks/shared-scripts";
+      i = "invoke";
     };
+
+    # For some reason _complete_alias version works only after `inv` or `invoke` are triggered
+    # at least once. There's also a _complete_invoke function but don't know
+    # how to get into it.
+    bashrcExtra = ''
+      eval "$(invoke --print-completion-script bash)"
+      complete -F _complete_invoke i
+    '';
   };
 }

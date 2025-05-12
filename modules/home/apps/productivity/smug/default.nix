638fe9664e36b622d96544233e3cb457ca0e8039
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-05-12 12:40:46 +0300

    [Fix](Smug): Pinned 0.3.3 version while issue with file flag is not fixed.
    https://github.com/ivaaaan/smug/issues/137

--- modules/home/apps/productivity/smug/default.nix
+++ modules/home/apps/productivity/smug/default.nix
@@ -10,9 +10,23 @@ lib.${namespace}.mkIfEnabled {
   category = "productivity";
   name = "smug";
 }
-{
-  home.packages = with pkgs; [
-    smug
+(let
+  # On 0.3.5 and 0.3.6 --file attribute doesn't work.
+  # https://github.com/ivaaaan/smug/issues/137
+  smug-0_3_3 = pkgs.smug.overrideAttrs (oldAttrs: rec {
+    pname = "smug";
+    version = "0.3.3";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "ivaaaan";
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "sha256-dQp9Ov8Si9DfziVtX3dXsJg+BNKYOoL9/WwdalQ5TVw=";
+    };
+  });
+in {
+  home.packages = [
+    smug-0_3_3
   ];
 
   home.file = {
@@ -24,4 +38,4 @@ lib.${namespace}.mkIfEnabled {
       out-of-store = true;
     };
   };
-}
+})

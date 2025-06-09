b636155e93999a4d7f0bae59680333f51ea4e236
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-10 00:42:18 +0300

    [Enh](Darwin): Added projects .envrc population.

--- modules/home/bookmarks/darwin/default.nix
+++ modules/home/bookmarks/darwin/default.nix
@@ -1,19 +1,66 @@
-{ config, lib, namespace, ... }:
-lib.${namespace}.mkIfEnabled {
-  inherit config;
-  category = "bookmarks";
-  name = "darwin";
-  extraPredicate = lib.${namespace}.mkIfDarwin;
-}
 {
-  home.file = builtins.mapAttrs
-    (name: value: { source = config.lib.file.mkOutOfStoreSymlink value; })
-    (
-      lib.attrsets.concatMapAttrs (name: value: { ".bookmarks/${name}" = value; } )
+  config,
+  lib,
+  namespace,
+  ...
+}: let
+  envrc = /*bash*/''
+    source_env_if_exists ../.envrc
+
+    # vi:ft=bash
+  '';
+in
+  lib.${namespace}.mkIfEnabled {
+    inherit config;
+    category = "bookmarks";
+    name = "darwin";
+    extraPredicate = lib.${namespace}.mkIfDarwin;
+  }
+  {
+    # See `darwinConfigurations.creamsoda.config.home-manager.users.apakalo.home.file.`
+    # in nix-repl to debug.
+    home.file =
+      builtins.mapAttrs
+      (name: value: {source = config.lib.file.mkOutOfStoreSymlink value;})
       (
+        lib.attrsets.concatMapAttrs (name: value: {".bookmarks/${name}" = value;})
         {
           "ChronoIndex" = ~/ChronoIndex;
         }
       )
-    );
-}
+      // (
+        lib.attrsets.concatMapAttrs (name: value: {"${config.home.homeDirectory}/Projects/--professional/Rutube__/Woodpecker/${name}" = value;})
+        (
+          {
+            ".envrc".text =
+              /*
+              bash
+              */
+              ''
+                dotenv ./.env.dev
+
+                # vi:ft=bash
+              '';
+            ".env.dev".text =
+              /*
+              bash
+              */
+              ''
+                # Disable auto-setting of `packageManager` when corepack is enabled.
+                COREPACK_ENABLE_AUTO_PIN=0
+
+                # vi:ft=bash
+              '';
+          }
+          // lib.attrsets.concatMapAttrs (projectName: value: {"${projectName}/.envrc".text = envrc;})
+          {
+            "CurrentTask" = "";
+            "CurrentTask1" = "";
+            "Epic" = "";
+            "Main" = "";
+            "Release" = "";
+            "Review" = "";
+          }
+        )
+      );
+  }

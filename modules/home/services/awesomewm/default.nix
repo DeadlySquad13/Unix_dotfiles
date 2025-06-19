42d04598c0d9f57d4c864972c3337c4541c2e407
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-19 23:54:03 +0300

    [Fix](AwesomeWm): Added config to hom.file.

--- modules/home/services/awesomewm/default.nix
+++ modules/home/services/awesomewm/default.nix
@@ -11,11 +11,13 @@ lib.${namespace}.mkIfEnabled {
   name = "awesomewm";
 }
 {
-  # Prod.
-  ".config/awesome".source = pkgs.fetchFromGitHub {
-    owner = "DeadlySquad13";
-    repo = "AwesomeWm_config";
-    rev = "eccb523915a965a6b2c6eacb0c6967d39bba5e67";
-    hash = "sha256-0ZAthPoBncL+jxq8sUkGmLoTdrNdrGX1cO0CSCaPmJc=";
+  home.file = {
+    # Prod.
+    ".config/awesome".source = pkgs.fetchFromGitHub {
+      owner = "DeadlySquad13";
+      repo = "AwesomeWm_config";
+      rev = "eccb523915a965a6b2c6eacb0c6967d39bba5e67";
+      hash = "sha256-0ZAthPoBncL+jxq8sUkGmLoTdrNdrGX1cO0CSCaPmJc=";
+    };
   };
 }

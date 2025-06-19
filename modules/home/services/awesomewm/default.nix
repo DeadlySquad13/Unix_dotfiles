9e6d663dae300c0475ab99ddb83b4ce9881c8170
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-19 23:36:33 +0300

    [Enh]: Init new system and home @cake.

---
+++ modules/home/services/awesomewm/default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  lib,
+  namespace,
+  config,
+  ...
+}:
+lib.${namespace}.mkIfEnabled {
+  inherit config;
+  category = "services";
+  name = "awesomewm";
+}
+{
+  # Prod.
+  ".config/awesome".source = pkgs.fetchFromGitHub {
+    owner = "DeadlySquad13";
+    repo = "AwesomeWm_config";
+    rev = "eccb523915a965a6b2c6eacb0c6967d39bba5e67";
+    hash = "sha256-0ZAthPoBncL+jxq8sUkGmLoTdrNdrGX1cO0CSCaPmJc=";
+  };
+}

004d9be26550acf1aa1ee8c707a78c090462271e
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-27 15:59:22 +0300

    [Enh](bookmarks): Added sourcing via source function from lib.

--- modules/home/bookmarks/darwin/default.nix
+++ modules/home/bookmarks/darwin/default.nix
@@ -4,11 +4,75 @@
   namespace,
   ...
 }: let
-  envrc = /*bash*/''
-    source_env_if_exists ../.envrc
+  inherit (lib.${namespace}) source;
 
-    # vi:ft=bash
-  '';
+  # In most cases we jump directly into folder. This way common .envrc wouldn't
+  # be hooked. This envrc allows us to source it from a central point in each
+  # subdirectory.
+  envrc =
+    /*
+    bash
+    */
+    ''
+      source_env_if_exists ../.envrc
+
+      # vi:ft=bash
+    '';
+
+  woodpecker-path = "${config.home.homeDirectory}/Projects/--professional/Rutube__/Woodpecker";
+  # Project local disable of automatic corepack pinning of package manager version.
+  woodpecker-corepack =
+    lib.attrsets.concatMapAttrs
+    (name: value: {"${woodpecker-path}/${name}" = value;})
+    (
+      {
+        ".envrc".text =
+          /*
+          bash
+          */
+          ''
+            dotenv ./.env.dev
+
+            # vi:ft=bash
+          '';
+        ".env.dev".text =
+          /*
+          bash
+          */
+          ''
+            # Disable auto-setting of `packageManager` when corepack is enabled.
+            COREPACK_ENABLE_AUTO_PIN=0
+
+            # vi:ft=bash
+          '';
+      }
+      # Iterating over all worktrees.
+      # TODO: Ideally should be based on current worktree structure, not just
+      # hardcoded paths.
+      // lib.attrsets.concatMapAttrs (projectName: value: {"${projectName}/.envrc".text = envrc;})
+      {
+        "CurrentTask" = "";
+        "CurrentTask1" = "";
+        "Epic" = "";
+        "Main" = ""; # Actually symlink to Woodpecker.git. Woodpecker.git is also symlinked to Master.
+        "Release" = "";
+        "Review" = "";
+      }
+      // {
+        # Local for repository, personal to our workflow gitignore file.
+        # https://git-scm.com/docs/gitignore,
+        # https://stackoverflow.com/questions/1753070/how-do-i-configure-git-to-ignore-some-files-locally
+        "Woodpecker.git/.git/info/exclude".text =
+          /*
+          gitignore
+          */
+          ''
+            # Usually not ignored, but our members don't use it, it's for our personal
+            # solution of a corepack issue.
+            .envrc
+          '';
+      }
+    );
 in
   lib.${namespace}.mkIfEnabled {
     inherit config;
@@ -21,46 +85,15 @@ in
     # in nix-repl to debug.
     home.file =
       builtins.mapAttrs
-      (name: value: {source = config.lib.file.mkOutOfStoreSymlink value;})
+      (name: value: source { inherit config; get-path = _p: value; out-of-store = true; })
       (
         lib.attrsets.concatMapAttrs (name: value: {".bookmarks/${name}" = value;})
         {
-          "ChronoIndex" = ~/ChronoIndex;
+          "ChronoIndex" = "~/ChronoIndex";
+          "KnowledgeBase__Data" = "~/KnowledgeBase__Data";
+          "kbd" = "~/KnowledgeBase__Data";
+          "kbn" = "~/KnowledgeBase__Data/Notes";
         }
       )
-      // (
-        lib.attrsets.concatMapAttrs (name: value: {"${config.home.homeDirectory}/Projects/--professional/Rutube__/Woodpecker/${name}" = value;})
-        (
-          {
-            ".envrc".text =
-              /*
-              bash
-              */
-              ''
-                dotenv ./.env.dev
-
-                # vi:ft=bash
-              '';
-            ".env.dev".text =
-              /*
-              bash
-              */
-              ''
-                # Disable auto-setting of `packageManager` when corepack is enabled.
-                COREPACK_ENABLE_AUTO_PIN=0
-
-                # vi:ft=bash
-              '';
-          }
-          // lib.attrsets.concatMapAttrs (projectName: value: {"${projectName}/.envrc".text = envrc;})
-          {
-            "CurrentTask" = "";
-            "CurrentTask1" = "";
-            "Epic" = "";
-            "Main" = "";
-            "Release" = "";
-            "Review" = "";
-          }
-        )
-      );
+      // woodpecker-corepack;
   }

07c77919b59cad5210717be0c21ee6012403cc0d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-06-29 15:12:25 +0000

    [Enh](Bugwarrior): Added Logseq service.

--- modules/home/apps/productivity/bugwarrior/default.nix
+++ modules/home/apps/productivity/bugwarrior/default.nix
@@ -19,6 +19,28 @@ lib.${namespace}.mkIfEnabled {
     # python311Packages.bugwarrior
     python3Packages.bugwarrior-develop
   ];
+  programs.taskwarrior = {
+    extraConfig =
+      /*
+      ini
+      */
+      ''
+        # Bugwarrior UDAs
+        uda.logseqid.type=string
+        uda.logseqid.label=Logseq ID
+        uda.logsequuid.type=string
+        uda.logsequuid.label=Logseq UUID
+        uda.logseqstate.type=string
+        uda.logseqstate.label=Logseq State
+        uda.logseqtitle.type=string
+        uda.logseqtitle.label=Logseq Title
+        uda.logseqdone.type=date
+        uda.logseqdone.label=Logseq Done
+        uda.logsequri.type=string
+        uda.logsequri.label=Logseq URI
+        # END Bugwarrior UDAs
+      '';
+  };
 
   home.file = {
     ".config/bugwarrior/bugwarrior.toml".text =
@@ -27,14 +49,19 @@ lib.${namespace}.mkIfEnabled {
       */
       ''
         [general]
+        taskrc = "~/.config/task/taskrc"
         targets = ["my_logseq_graph"]
+        static_fields = ["project"]
 
         # When true will include a link to the ticket as an annotation.
         annotation_links = true
 
         [my_logseq_graph]
         service = "logseq"
-        token = "mybugwarrioraccesstoken"
+        token = "token"
+        host = "172.18.208.1"
+        task_state = "WAITING, DOING"
+        project_template = ""
       '';
   };
 }

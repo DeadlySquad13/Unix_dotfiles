c1b8f8e8c3ab593b1a2c85f250a72c8d0c19bb91
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-08-24 12:42:01 +0300

    [Fix](Logseq): Added desktop entry to enable web-clipper.

--- modules/home/apps/productivity/logseq/default.nix
+++ modules/home/apps/productivity/logseq/default.nix
@@ -14,4 +14,19 @@ lib.${namespace}.mkIfEnabled {
   home.packages = with pkgs; [
     ds-omega.gl-logseq
   ];
+  # Very similar to obsidian.
+  # Needed for web clipper on Linux.
+  # There's very similar entry in [source code](https://github.com/NixOS/nixpkgs/blob/20075955deac2583bb12f07151c2df830ef346b4/pkgs/by-name/lo/logseq/package.nix#L267C3-L267C15)
+  # but it doesn't work. Maybe my gl overlay ruins it...
+  xdg.desktopEntries.logesq = {
+      name = "Logseq";
+      desktopName = "Logseq";
+      exec = "logseq %U";
+      terminal = false;
+      icon = "logseq";
+      startupWMClass = "Logseq";
+      comment = "A privacy-first, open-source platform for knowledge management and collaboration.";
+      mimeType = [ "x-scheme-handler/logseq" ];
+      categories = [ "Utility" ];
+  };
 }

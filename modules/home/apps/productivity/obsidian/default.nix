89f4ebc568c41065394ac80ec2d2165a27a0aebf
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-08-24 12:32:27 +0300

    [Fix](Obsidian): Added desktop entry to enable web-clipper.

--- modules/home/apps/productivity/obsidian/default.nix
+++ modules/home/apps/productivity/obsidian/default.nix
@@ -14,4 +14,15 @@ lib.${namespace}.mkIfEnabled {
   home.packages = with pkgs; [
     ds-omega.gl-obsidian
   ];
+  # Needed for [web clipper](https://help.obsidian.md/web-clipper/troubleshoot#Obsidian+does+not+open) on Linux.
+  # There's very similar entry in [source code](https://github.com/NixOS/nixpkgs/blob/20075955deac2583bb12f07151c2df830ef346b4/pkgs/by-name/ob/obsidian/package.nix#L49)
+  # but it doesn't work. Maybe my gl overlay ruins it...
+  xdg.desktopEntries.obsidian = {
+    name = "obsidian";
+    comment = "Knowledge base";
+    icon = "obsidian";
+    exec = "obsidian %u";
+    categories = [ "Office" ];
+    mimeType = [ "x-scheme-handler/obsidian" ];
+  };
 }

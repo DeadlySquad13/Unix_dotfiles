cae03b5f2faba0c8b7c6df459ec6a0e5c6a121a2
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-09-16 00:13:33 +0300

    [Chore](XdgAutostart): Updated v2rayN desktop file.

--- modules/home/apps/gui-utility/xdg-autostart/default.nix
+++ modules/home/apps/gui-utility/xdg-autostart/default.nix
@@ -21,10 +21,27 @@ lib.${namespace}.mkIfEnabled {
   home.file = {
     # Applications that are made available through our system-package manager
     # means or from application itself when not packaged via Nix.
-    ".config/autostart/v2rayN.desktop".source = /usr/share/applications/v2rayN.desktop;
+    # ".config/autostart/v2rayN.desktop".source = /usr/share/applications/v2rayN.desktop;
+    # Autostart (and stay minimized but I guess it's not reflected here).
+    ".config/autostart/v2rayN.desktop".text = /*desktop*/ ''
+      [Desktop Entry]
+      Type=Application
+      Exec=/usr/lib/v2rayN/v2rayN
+      Hidden=false
+      NoDisplay=false
+      X-GNOME-Autostart-enabled=true
+      Name[en_US]=v2rayN
+      Name=v2rayN
+      Comment[en_US]=v2rayN
+      Comment=v2rayN
+    '';
 
     # QUESTION: Better to be moved to ferdium directly but it requires that
     # this module is loaded. How to organize such cases better?
+    # REFACTOR: Can be made via xdg.desktopEntries? Or if it has hardcoded
+    # location, then generation desktopEntry in autostart folder via
+    # makeDesktopItem ([Source](https://github.com/NixOS/nixpkgs/blob/07dec8191796fa9cd8806b1bac24a49ec05e5d2d/pkgs/build-support/make-desktopitem/default.nix),
+    # [Example](https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/ob/obsidian/package.nix#L7))
     ".config/autostart/ferdium.desktop".text = /*desktop*/ ''
       [Desktop Entry]
       Type=Application

f86fc095756cad45081395f856b9be8ecb15ba95
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-09-20 19:33:29 +0300

    [Enh](@cake): Init audio setup.
    Only low level without widgets.
    <a href="zotero://select/library/items/@/NixOSWikiAudio">NixOS
    Wiki:Audio - NixOS Wiki</a>

--- systems/x86_64-linux/cake/default.nix
+++ systems/x86_64-linux/cake/default.nix
@@ -122,6 +122,30 @@ in {
     # ];
   };
 
+  #
+  # Audio
+  #
+  hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+
+  security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Uncomment the following line if you want to use JACK applications
+    # jack.enable = true;
+  };
+
+  #
+  # Bluetooth
+  #
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = false; # To save on power.
+  };
+
   services.sshd.enable = true;
 
   nix.settings = {

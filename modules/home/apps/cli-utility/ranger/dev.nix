1a4ef174ef383e1e65b94feb2c0a1c2084b4160b
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-09-28 11:26:28 +0300

    [Chore](@cake): Enabled dev variants, made a hack for stage variants on a level of home config.

--- modules/home/apps/cli-utility/ranger/dev.nix
+++ modules/home/apps/cli-utility/ranger/dev.nix
@@ -13,19 +13,19 @@ in
     extraPredicate = mkIfDevEnabled;
   }
   {
-    # home.file = {
-    #   # Dev.
-    #   ".local/dotfiles-/_configs/ranger/-dev/commands.py" = lib.${namespace}.source {
-    #     inherit config;
-    #     get-path = p: "${p.shared-configs}/Wsl2_dotfiles/stow_home/ranger/.config/ranger/commands.py";
-    #     out-of-store = true;
-    #   };
-    #   ".local/dotfiles-/_configs/ranger/-dev/rifle.conf" = lib.${namespace}.source {
-    #     inherit config;
-    #     get-path = p: "${p.shared-configs}/Wsl2_dotfiles/stow_home/ranger/.config/ranger/rifle.conf";
-    #     out-of-store = true;
-    #   };
-    # };
+    home.file = {
+      # Dev.
+      ".local/dotfiles-/_configs/ranger/-dev/commands.py" = lib.${namespace}.source {
+        inherit config;
+        get-path = p: "${p.shared-configs}/Wsl2_dotfiles/stow_home/ranger/.config/ranger/commands.py";
+        out-of-store = true;
+      };
+      ".local/dotfiles-/_configs/ranger/-dev/rifle.conf" = lib.${namespace}.source {
+        inherit config;
+        get-path = p: "${p.shared-configs}/Wsl2_dotfiles/stow_home/ranger/.config/ranger/rifle.conf";
+        out-of-store = true;
+      };
+    };
 
     home.shellAliases = {
       ranger-dev = "ranger --confdir=${config.home.homeDirectory}/.local/dotfiles-/_configs/ranger/-dev";

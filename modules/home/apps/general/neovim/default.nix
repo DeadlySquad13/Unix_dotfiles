a40a1c4c752a9f927817d3caca62dcf4e673d208
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-10-09 14:25:43 +0300

    [Fix](NeoVim): Removed xclip when it's not needed.
    It was ruining bash yank alias. Fixed in bash config it too.

--- modules/home/apps/general/neovim/default.nix
+++ modules/home/apps/general/neovim/default.nix
@@ -6,40 +6,59 @@
   namespace,
   config,
   ...
-}:
-{
-  imports = [
-    # Overrides next modules.
-    ./dev.nix
-    ./stage.nix
-    ./prod.nix
-  ];
-}
-// lib.${namespace}.mkIfEnabled {
-  inherit config;
-  category = "general";
-  name = "neovim";
-}
-{
-  home.packages = with pkgs; [
-    neovim
-    xclip
-    # REFACTOR: Required on nixos but not necessary on other systems.
-    zig
-  ];
-  # TODO: Move all these to layer.
-  # imports =
-  # [
-  #     # Was required for rest.nvim (for luarocks to be more specific).
-  #     ../unzip/default.nix
+}: let
+  inherit (lib.ds-omega) mkIfLinux;
+  inherit (lib) mkIf mkMerge;
+in
+  {
+    imports = [
+      # Overrides next modules.
+      ./dev.nix
+      ./stage.nix
+      ./prod.nix
+    ];
+  }
+  // lib.${namespace}.mkIfEnabled
+  {
+    inherit config;
+    category = "general";
+    name = "neovim";
+  }
+  {
+    home = mkMerge [
+      {
+        packages = with pkgs; [
+          neovim
+          # REFACTOR: *Required* on nixos but not necessary on other systems.
+          zig
+        ];
+      }
+      (mkIf (mkIfLinux {}) {
+        packages = with pkgs; [
+          xclip
+        ];
+      })
+    ];
+    # (
+    #     mkIf (mkIfLinux {}) [
+    #       xclip
+    #       zig
+    #     ]
+    #   )
+    # );
+    # TODO: Move all these to layer.
+    # imports =
+    # [
+    #     # Was required for rest.nvim (for luarocks to be more specific).
+    #     ../unzip/default.nix
 
-  #     # TODO: Move to a separate layer.
-  #     ../nil/default.nix
-  #     ../statix/default.nix
-  #     ../alejandra/default.nix
+    #     # TODO: Move to a separate layer.
+    #     ../nil/default.nix
+    #     ../statix/default.nix
+    #     ../alejandra/default.nix
 
-  #     # TODO: Move to a separate layer.
-  #     # Used for sniprun REPLs.
-  #     ../deno/default.nix
-  # ];
-}
+    #     # TODO: Move to a separate layer.
+    #     # Used for sniprun REPLs.
+    #     ../deno/default.nix
+    # ];
+  }

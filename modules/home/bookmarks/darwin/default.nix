b620a7fdd096bec7da927a01bb5fcc49dad9c43a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-12-23 18:38:38 +0300

    [Enh](Darwin__Bookmarks): Added multi-project setup.

--- modules/home/bookmarks/darwin/default.nix
+++ modules/home/bookmarks/darwin/default.nix
@@ -15,9 +15,7 @@
   # (outside of worktree).
   # 2. We may need to separate them per worktree in the future
   envrc =
-    /*
-    bash
-    */
+    # bash
     ''
       # Flake must be inside git repo (and not ignored), otherwise it would need to clone
       # a whole repo to the nix store. Hence we move it to parent dir with
@@ -29,65 +27,76 @@
       # vi:ft=bash
     '';
 
-  woodpecker-path = "${config.home.homeDirectory}/Projects/--professional/Rutube__/Woodpecker";
+  projects-root = "${config.home.homeDirectory}/Projects";
+  # TODO: Get from
+  # rut-Programming_dotfiles/roles/repositories/vars/main.yml
+  projects = [
+    {
+      suite-name = "Woodpecker";
+      path = "${projects-root}/--professional/Rutube__/Woodpecker";
+    }
+    {
+      suite-name = "Premium";
+      path = "${projects-root}/--professional/Rutube__/Premium";
+    }
+  ];
+
   # Project local disable of automatic corepack pinning of package manager version.
-  woodpecker-corepack =
-    lib.attrsets.concatMapAttrs
-    (name: value: {"${woodpecker-path}/${name}" = value;})
-    (
-      {
-        ".envrc".text =
-          /*
-          bash
-          */
-          ''
-            dotenv ./.env.dev
-            # TODO: Remove temporary workaround when it's properly fixed https://github.com/NixOS/nixpkgs/issues/376958
-            unset DEVELOPER_DIR
+  project-structures =
+    lib.lists.foldl (
+      acc: project:
+        acc
+        // lib.attrsets.concatMapAttrs (name: value: {"${project.path}/${name}" = value;}) (
+          {
+            ".envrc".text =
+              # bash
+              ''
+                dotenv ./.env.dev
+                # TODO: Remove temporary workaround when it's properly fixed https://github.com/NixOS/nixpkgs/issues/376958
+                unset DEVELOPER_DIR
 
-            # vi:ft=bash
-          '';
-        ".env.dev".text =
-          /*
-          bash
-          */
-          ''
-            # Disable auto-setting of `packageManager` when corepack is enabled.
-            COREPACK_ENABLE_AUTO_PIN=0
+                # vi:ft=bash
+              '';
+            ".env.dev".text =
+              # bash
+              ''
+                # Disable auto-setting of `packageManager` when corepack is enabled.
+                COREPACK_ENABLE_AUTO_PIN=0
 
-            # vi:ft=bash
-          '';
-      }
-      # Iterating over all worktrees.
-      # TODO: Ideally should be based on current worktree structure, not just
-      # hardcoded paths.
-      // lib.attrsets.concatMapAttrs (projectName: value: {"${projectName}/.envrc".text = envrc;})
-      {
-        "CurrentTask" = "";
-        "CurrentTask1" = "";
-        "Epic" = "";
-        "Main" = ""; # Actually symlink to Woodpecker.git. Woodpecker.git is also symlinked to Master.
-        "Release" = "";
-        "Review" = "";
-      }
-      // {
-        # Local for repository, personal to our workflow gitignore file.
-        # https://git-scm.com/docs/gitignore,
-        # https://stackoverflow.com/questions/1753070/how-do-i-configure-git-to-ignore-some-files-locally
-        "Woodpecker.git/.git/info/exclude".text =
-          /*
-          gitignore
-          */
-          ''
-            # Usually not ignored, but our members don't use it, it's for our personal
-            # solution of a corepack issue and flake configs.
-            .envrc
-            .direnv
-          '';
-      }
-    );
+                # vi:ft=bash
+              '';
+          }
+          # Iterating over all worktrees.
+          # TODO: Ideally should be based on current worktree structure, not just
+          # hardcoded paths.
+          # TODO: Separate list based on project.
+          // lib.attrsets.concatMapAttrs (projectName: value: {"${projectName}/.envrc".text = envrc;}) {
+            "CurrentTask" = "";
+            "CurrentTask1" = "";
+            "Epic" = "";
+            "Main" = ""; # Actually symlink to <ProjectSuiteName>.git. <ProjectSuiteName>.git is also symlinked to Master.
+            "Release" = "";
+            "Review" = "";
+          }
+          // {
+            # Local for repository, personal to our workflow gitignore file.
+            # https://git-scm.com/docs/gitignore,
+            # https://stackoverflow.com/questions/1753070/how-do-i-configure-git-to-ignore-some-files-locally
+            "${project.suite-name}.git/.git/info/exclude".text =
+              # gitignore
+              ''
+                # Usually not ignored, but our members don't use it, it's for our personal
+                # solution of a corepack issue and flake configs.
+                .envrc
+                .direnv
+              '';
+          }
+        )
+    ) {}
+    projects;
 in
-  lib.${namespace}.mkIfEnabled {
+  lib.${namespace}.mkIfEnabled
+  {
     inherit config;
     category = "bookmarks";
     name = "darwin";
@@ -98,10 +107,16 @@ in
     # in nix-repl to debug.
     home.file =
       builtins.mapAttrs
-      (name: value: source { inherit config; get-path = _p: value; out-of-store = true; })
       (
-        lib.attrsets.concatMapAttrs (name: value: {".bookmarks/${name}" = value;})
-        {
+        name: value:
+          source {
+            inherit config;
+            get-path = _p: value;
+            out-of-store = true;
+          }
+      )
+      (
+        lib.attrsets.concatMapAttrs (name: value: {".bookmarks/${name}" = value;}) {
           "ChronoIndex" = "~/ChronoIndex";
           "KnowledgeBase__Data" = "~/KnowledgeBase__Data";
           "kbd" = "~/KnowledgeBase__Data";
@@ -109,5 +124,5 @@ in
           "shared-configs" = paths.shared-configs;
         }
       )
-      // woodpecker-corepack;
+      // project-structures;
   }

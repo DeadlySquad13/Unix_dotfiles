288cb7100980cb8572a86d369f4011a6c4c1d730
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-02-01 18:39:27 +0300

    [Enh]: Init @NikolaiGogol for testing NixOs in Docker container.
    Via https://github.com/skiffos/docker-nixos.

---
+++ homes/x86_64-linux/ds13@NikolaiGogol/default.nix
@@ -0,0 +1,153 @@
+{
+  # Snowfall Lib provides a customized `lib` instance with access to your flake's library
+  # as well as the libraries available from your flake's inputs.
+  lib,
+  # An instance of `pkgs` with your overlays and packages applied is also available.
+  pkgs,
+  # You also have access to your flake's inputs.
+  inputs,
+  # Additional metadata is provided by Snowfall Lib.
+  namespace, # The namespace used for your flake, defaulting to "internal" if not set.
+  home, # The home architecture for this host (eg. `x86_64-linux`).
+  target, # The Snowfall Lib target for this home (eg. `x86_64-home`).
+  format, # A normalized name for the home target (eg. `home`).
+  virtual, # A boolean to determine whether this home is a virtual target using nixos-generators.
+  host, # The host name for this home.
+  # All other arguments come from the home home.
+  config,
+  ...
+}: let
+  inherit (lib.${namespace}) disabled enabled;
+in {
+  home = {
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "24.11";
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "ds13";
+    homeDirectory = "/home/ds13";
+  };
+
+  lib.${namespace} = {
+    paths =
+      rec {
+        # config = ~/.config;
+        kbd = ~/.bookmarks/kbn;
+        kbn = ~/.bookmarks/kbn;
+        # projects = ~/Projects;
+
+        dotfiles = "~/.local/dotfiles-";
+
+        # TODO: Make like in @creamsoda.
+        # shared-dotfiles = "${dotfiles}/shared-";
+        shared-configs = "~/.bookmarks/shared-configs";
+        shared-scripts = "~/.bookmarks/shared-scripts";
+
+        # TODO: Append home- like in @creamsoda.
+        # As far as I can tell, we would need at least change paths in Ansible
+        # Keyszer templates.
+        home-dotfiles = "${dotfiles}";
+        home-configs = "${home-dotfiles}/_configs";
+        home-scripts = "${home-dotfiles}/_scripts";
+      };
+
+    # TODO: Add as default value.
+    # TODO: Move to a system level in a separate module? Why it's home
+    # dependent?
+    deploymentOptions = {
+      isDeployedFromDarwin = false;
+      isNixGlNotWorking = true;
+    };
+
+    modules = {
+      architecturing = {
+        enable = false;
+      };
+      cli-utility = {
+        enable = false;
+
+        bat = enabled;
+        bash = enabled;
+        zoxide = enabled;
+        invoke = enabled;
+        complete-alias = enabled;
+        fdupes = enabled;
+      };
+      development = {
+        enable = false;
+
+        git = enabled;
+        lazygit = enabled;
+        nodejs = enabled;
+        direnv = enabled;
+        pixi = disabled;
+        gh = enabled;
+        nix = enabled;
+      };
+      ecosystem = {
+        enable = false;
+      };
+      general = {
+        enable = true;
+
+        neovim = {
+          enabled = true; # TODO: Remove.
+          dev = true;
+          stage = false;
+        };
+
+        keepassxc = disabled;
+        ferdium = disabled;
+        telegram-desktop = disabled;
+        zathura = disabled;
+
+        wireguard-tools = disabled;
+        unzip = disabled;
+        vlc = disabled;
+        keychain = disabled;
+        numlockx = disabled;
+        obs-studio = disabled;
+        qbittorrent = disabled;
+        syncthing = enabled;
+        btop = disabled;
+        flatpak = disabled;
+        java-fonts-fix = disabled;
+        openvpn3 = disabled;
+        gpick = disabled;
+      };
+      gui-utility = {
+        enable = false;
+      };
+      productivity = {
+        enable = false;
+
+        taskwarrior = enabled;
+        bugwarrior = enabled;
+      };
+      misc = {
+        qmk = enabled;
+      };
+      writing = {
+        enable = false;
+
+        markdown-oxide = enabled;
+      };
+
+      tools = {
+        enabled = false;
+
+        ranger = {
+          enabled = false;
+          dev = false;
+          stage = false;
+        };
+      };
+    };
+  };
+}

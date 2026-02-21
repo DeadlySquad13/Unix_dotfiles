4c55b088b5aa4a3c481f63b521a0af55f65b9743
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-02-21 22:42:44 +0300

    [Chore](Smug): Updated version.
    Issue was fixed.

--- modules/home/apps/productivity/smug/default.nix
+++ modules/home/apps/productivity/smug/default.nix
@@ -10,23 +10,9 @@ lib.${namespace}.mkIfEnabled {
   category = "productivity";
   name = "smug";
 }
-(let
-  # On 0.3.5 and 0.3.6 --file attribute doesn't work.
-  # https://github.com/ivaaaan/smug/issues/137
-  smug-0_3_3 = pkgs.smug.overrideAttrs (oldAttrs: rec {
-    pname = "smug";
-    version = "0.3.3";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "ivaaaan";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-dQp9Ov8Si9DfziVtX3dXsJg+BNKYOoL9/WwdalQ5TVw=";
-    };
-  });
-in {
-  home.packages = [
-    smug-0_3_3
+{
+  home.packages = with pkgs; [
+    smug
   ];
 
   home.file = {
@@ -38,4 +24,4 @@ in {
       out-of-store = true;
     };
   };
-})
+}

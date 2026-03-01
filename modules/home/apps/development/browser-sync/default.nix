0915faaf3ccc6d1b9ed8a75e93d696ded9c3ce0a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-03-01 18:21:17 +0300

    [Chore](BrowserSync): Disabled because no longer maintained.

--- modules/home/apps/development/browser-sync/default.nix
+++ modules/home/apps/development/browser-sync/default.nix
@@ -12,6 +12,8 @@ lib.${namespace}.mkIfEnabled {
 }
 {
   home.packages = with pkgs; [
-    nodePackages_latest.browser-sync
+    # FIX: error: 'browser-sync' has been removed because it was unmaintained in nixpkgs
+    # I think we may just change it to something similar.
+    # nodePackages_latest.browser-sync
   ];
 }

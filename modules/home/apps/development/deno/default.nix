57b186ddbf86ab171001debc5f2b04c0e24711c9
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-03-01 18:21:40 +0300

    [Chore](Deno): Used version from stable as unstable is currently not building.

--- modules/home/apps/development/deno/default.nix
+++ modules/home/apps/development/deno/default.nix
@@ -1,17 +1,27 @@
 {
-  pkgs,
+  # pkgs,
   lib,
+  inputs,
   namespace,
   config,
   ...
-}:
-lib.${namespace}.mkIfEnabled {
-  inherit config;
-  category = "development";
-  name = "deno";
-}
-{
-  home.packages = with pkgs; [
-    deno
-  ];
-}
+}: let
+  # TODO: Remove once build error is fixed. Must be ok next time we update
+  # flake inputs.
+  pkgs-stable = import inputs.nixpkgs-stable {
+    config.permittedInsecurePackages = [
+      "electron-27.3.11"
+    ];
+  };
+in
+  lib.${namespace}.mkIfEnabled
+  {
+    inherit config;
+    category = "development";
+    name = "deno";
+  }
+  {
+    home.packages = with pkgs-stable; [
+      deno
+    ];
+  }

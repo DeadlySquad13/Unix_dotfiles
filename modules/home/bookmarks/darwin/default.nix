52e1c3225df1cfed3df39e7133b06255ef3eea0d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-03-17 15:09:03 +0300

    [Enh](Bookmarks): Added Nix and Smug _config mapping.

--- modules/home/bookmarks/darwin/default.nix
+++ modules/home/bookmarks/darwin/default.nix
@@ -28,27 +28,43 @@
     '';
 
   projects-root = "${config.home.homeDirectory}/Projects";
+  rutube-projects-root = "${projects-root}/--professional/Rutube__";
   # TODO: Get from
   # rut-Programming_dotfiles/roles/repositories/vars/main.yml
+  rutube-projects-worktrees = [
+    "CurrentTask"
+    "CurrentTask1"
+    "Epic"
+    "Main"
+    "Release"
+    "Review"
+  ];
+
   # INFO: path - location of the project. If it is located in different
   # directory, symlink should be added before running this flake. Otherwise
   # Nix will create a new directory under specified path.
+  # TODO: Get from
+  # rut-Programming_dotfiles/roles/repositories/vars/main.yml
   projects = [
     {
       suite-name = "Woodpecker";
-      path = "${projects-root}/--professional/Rutube__/Woodpecker";
+      path = "${rutube-projects-root}/Woodpecker";
+      worktrees = rutube-projects-worktrees;
     }
     {
       suite-name = "Premium";
-      path = "${projects-root}/--professional/Rutube__/Premium";
+      path = "${rutube-projects-root}/Premium";
+      worktrees = rutube-projects-worktrees;
     }
     {
       suite-name = "Raichu";
-      path = "${projects-root}/--professional/Rutube__/Raichu";
+      path = "${rutube-projects-root}/Raichu";
+      worktrees = rutube-projects-worktrees;
     }
     {
       suite-name = "ReleaseBuilder";
-      path = "${projects-root}/--professional/Rutube__/ReleaseBuilder";
+      path = "${rutube-projects-root}/ReleaseBuilder";
+      worktrees = rutube-projects-worktrees;
     }
   ];
 
@@ -57,6 +73,45 @@
     lib.lists.foldl (
       acc: project:
         acc
+        # Symlinks
+        // (
+          builtins.mapAttrs
+          (
+            name: value:
+              source {
+                inherit config;
+                get-path = _p: value;
+                out-of-store = true;
+              }
+          )
+          (
+            lib.attrsets.concatMapAttrs (name: value: {"${project.path}/${name}" = value;}) (
+              # - Nix
+              {
+                "_configs/Nix" = "${rutube-projects-root}/_configs/Nix__/${project.suite-name}";
+              }
+              # - Smug Git Worktrees.
+              // lib.lists.foldl (
+                acc: worktree: let
+                  # Choosing different configs.
+                  smug-worktree-configs-root = "${rutube-projects-root}/_configs/Smug__";
+                  project-specific-smug-worktree-configs = "${smug-worktree-configs-root}/${project.suite-name}";
+                  default-smug-worktree-configs = "${smug-worktree-configs-root}/-gitWorktrees";
+
+                  smug-worktree-configs =
+                    if lib.filesystem.pathIsDirectory project-specific-smug-worktree-configs
+                    then project-specific-smug-worktree-configs
+                    else default-smug-worktree-configs;
+                in
+                  acc
+                  // {
+                    "_configs/${worktree}.yml" = "${smug-worktree-configs}/${worktree}.yml";
+                  }
+              ) {}
+              (project.worktrees ? rutube-projects-worktrees)
+            )
+          )
+        )
         // lib.attrsets.concatMapAttrs (name: value: {"${project.path}/${name}" = value;}) (
           {
             ".envrc".text =

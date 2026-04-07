ff5b5edba5e4a12ea7a2ac8f4903f019b52393e4
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-04-07 23:02:12 +0300

    [Fix](Logseq): Set xdg desktop variable to resolve issues with "open in default app".

--- modules/home/apps/productivity/logseq/default.nix
+++ modules/home/apps/productivity/logseq/default.nix
@@ -14,17 +14,21 @@ lib.${namespace}.mkIfEnabled {
   home.packages = with pkgs; [
     ds-omega.gl-logseq
   ];
+
   # Very similar to obsidian.
   # Needed for web clipper on Linux.
   # There's very similar entry in [source code](https://github.com/NixOS/nixpkgs/blob/20075955deac2583bb12f07151c2df830ef346b4/pkgs/by-name/lo/logseq/package.nix#L267C3-L267C15)
   # but it doesn't work. Maybe my gl overlay ruins it...
-  xdg.desktopEntries.logesq = {
+  xdg.desktopEntries.Logseq = {
       name = "Logseq";
-      exec = "logseq %U";
+      # To fix logseq "Open with default app" on Linux without desktop manager.
+      # see: https://github.com/logseq/logseq/issues/11462
+      exec = "env XDG_CURRENT_DESKTOP=GNOME logseq %U";
       terminal = false;
       icon = "logseq";
       comment = "A privacy-first, open-source platform for knowledge management and collaboration.";
       mimeType = [ "x-scheme-handler/logseq" ];
       categories = [ "Utility" ];
   };
+
 }
--- packages/gl-logseq/default.nix
+++ packages/gl-logseq/default.nix
@@ -2,8 +2,7 @@
   inputs,
   writeShellApplication,
   pkgs,
-}:
-let
+}: let
   nixGLNvidia = pkgs.nixgl.auto.nixGLNvidia;
   pkgs-stable = import inputs.nixpkgs-stable {
     config.permittedInsecurePackages = [
@@ -12,12 +11,17 @@ let
   };
   logseq = pkgs-stable.logseq;
 in
-writeShellApplication {
-  name = "logseq";
-  runtimeInputs = [
-    logseq
-    nixGLNvidia
-  ];
-  text = ''exec -a logseq ${nixGLNvidia}/bin/${nixGLNvidia.name} logseq "$@"'';
-  meta.priority = (logseq.meta.priority or 5) - 1;
-}
+  writeShellApplication {
+    name = "logseq";
+    runtimeInputs = [
+      logseq
+      nixGLNvidia
+    ];
+    runtimeEnv = {
+      # To fix logseq "Open with default app" on Linux without desktop manager.
+      # see: https://github.com/logseq/logseq/issues/11462
+      XDG_CURRENT_DESKTOP = "GNOME";
+    };
+    text = ''exec -a logseq ${nixGLNvidia}/bin/${nixGLNvidia.name} logseq "$@"'';
+    meta.priority = (logseq.meta.priority or 5) - 1;
+  }

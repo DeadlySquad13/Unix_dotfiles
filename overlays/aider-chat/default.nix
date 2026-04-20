aa23423b7e9bc650d38d5a32355aa3757a487984
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-04-20 17:25:30 +0300

    [Fix](AiderChat): Disabled tests.

--- overlays/aider-chat/default.nix
+++ overlays/aider-chat/default.nix
@@ -2,13 +2,23 @@
   channels,
   inputs,
   ...
-}: final: prev: {
-  ds-omega-aider-chat = inputs.nixpkgs.legacyPackages.${prev.system}.aider-chat.withOptional {
-    withBrowser = true;
-    # TODO: Issues with spacy installation, update nixpkgs to 26.0.0
-    # See: https://github.com/NixOS/nixpkgs/pull/466482
-    withHelp = false;
-    withPlaywright = true;
-    withBedrock = false; # Not used.
-  };
-}
+}: final: prev: (
+  let
+    inherit (inputs.nixpkgs.legacyPackages.${prev.system}) aider-chat;
+  in {
+    ds-omega-aider-chat =
+      (aider-chat.withOptional {
+        withBrowser = true;
+        # TODO: Issues with spacy installation, update nixpkgs to 26.0.0
+        # See: https://github.com/NixOS/nixpkgs/pull/466482
+        withHelp = false;
+        withPlaywright = true;
+        withBedrock = false; # Not used.
+      }).overridePythonAttrs
+      (old: {
+        # As of 0.86.1 some tests related to `max_input_tokens` are impure and require network connection.
+        # We could use `--impure --sandbox false` but I decided to ditch it.
+        doCheck = false;
+      });
+  }
+)

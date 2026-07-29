f6e52d6e321c09b38611741aaea5339fca0c7d2d
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-07-29 17:34:53 +0300

    [Fix](Opencode__Tools): Added extension to the typescript files.

--- modules/home/apps/ai-assistance/opencode/tools/shared.nix
+++ modules/home/apps/ai-assistance/opencode/tools/shared.nix
@@ -3,30 +3,48 @@
   config,
   namespace,
   ...
-}:
-let
-  inherit (lib.${namespace}) prefixKeys prefixValues sourceAttrs;
+}: let
+  inherit
+    (lib.${namespace})
+    concatParentToPathKeys
+    mapValues
+    sourceAttrs
+    suffixKeys
+    ;
   opencodeToolsPath = ".config/opencode/tool";
   toolsPath = "/home/ds13/Projects/--personal/AiAssistance__/_tools";
   bindingsPath = /home/ds13/Projects/--personal/AiAssistance__/AiAssistance_agents__TypeScript_bindings;
-in
-{
+  # TODO: We may eventually move from flat structure to nested. We would need
+  # to process folders too.
+  typeScriptSourceFiles = lib.pipe config.programs.opencode.toolWrappers [
+    (toolWrappers: lib.genAttrs toolWrappers (t: "${t}.ts"))
+    (suffixKeys ".ts")
+  ];
+  sourceFiles =
+    typeScriptSourceFiles
+    // {
+      "package.json" = "package.json";
+    };
+  homeFileEntriesForSourceFiles = lib.pipe sourceFiles [
+    (concatParentToPathKeys opencodeToolsPath)
+    (mapValues (v: "${toolsPath}/${v}"))
+    (sourceAttrs {
+      inherit config;
+      out-of-store = true;
+    })
+  ];
+in {
   options.programs.opencode.toolWrappers = lib.mkOption {
     type = lib.types.listOf lib.types.str;
-    default = [ ];
+    default = [];
     description = "List of tools that need NODE_PATH wrapper";
   };
 
-  config = lib.mkIf (config.programs.opencode.toolWrappers != [ ]) {
-    home.file = lib.pipe null [
-      (_: lib.genAttrs config.programs.opencode.toolWrappers (t: "${t}.ts")
-        // { "package.json" = "package.json"; })
-      (prefixKeys opencodeToolsPath)
-      (prefixValues (v: "${toolsPath}/${v}"))
-      (sourceAttrs { inherit config; out-of-store = true; })
-    ]
-    // {
-      "${opencodeToolsPath}/node_modules/@aiassistance-agents/typescript-bindings".source = bindingsPath;
-    };
+  config = lib.mkIf (config.programs.opencode.toolWrappers != []) {
+    home.file =
+      homeFileEntriesForSourceFiles
+      // {
+        "${opencodeToolsPath}/node_modules/@aiassistance-agents/typescript-bindings".source = bindingsPath;
+      };
   };
 }

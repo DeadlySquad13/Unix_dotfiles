69c0f89b08c6df6f58f813a184143ec24aa4c404
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2026-07-29 17:34:24 +0300

    [Enh](Lib__Attrsets): Added different versions of value and key mapping, organized them logically.

--- lib/attrsets/default.nix
+++ lib/attrsets/default.nix
@@ -4,9 +4,33 @@
 }: let
   inherit (lib.${namespace}) source;
 in {
-  prefixValues = f: attrs: lib.attrsets.mapAttrs (_name: f) attrs;
+  # # Values transformations.
+  # Simpler version of a `mapAttrs`: function `f` accepts only value as an argument.
+  mapValues = f: attrs: lib.attrsets.mapAttrs (_name: f) attrs;
 
-  prefixKeys = prefix: attrs: lib.attrsets.concatMapAttrs (name: value: {"${prefix}/${name}" = value;}) attrs;
+  # Simpler version of a `mapAttrs`: accepts only prefix as an argument and simply concats prefix and value.
+  prefixValues = prefix: attrs: lib.attrsets.mapAttrs (_name: value: "${prefix}${value}") attrs;
+  # Simpler version of a `mapAttrs`: accepts only suffix as an argument and simply concats value and suffix.
+  suffixValues = suffix: attrs: lib.attrsets.mapAttrs (_name: value: "${value}${suffix}") attrs;
+
+  # Concat to path-like values (string actually).
+  concatParentToPathValues = prefix: attrs: lib.attrsets.mapAttrs (_name: value: "${prefix}/${value}") attrs;
+  # Concat to path-like values (string actually).
+  concatChildToParentValues = suffix: attrs: lib.attrsets.mapAttrs (_name: value: "${value}/${suffix}") attrs;
+
+  # # Keys transformations.
+  # Simpler version of a `concatMapAttrs`: function `f` accepts only key as an argument.
+  mapKeys = f: attrs: lib.attrsets.concatMapAttrs (name: value: {"${f name}" = value;}) attrs;
+
+  # Simpler version of a `concatMapAttrs`: accepts only prefix as an argument and simply concats prefix and value.
+  prefixKeys = prefix: attrs: lib.attrsets.concatMapAttrs (name: value: {"${prefix}${name}" = value;}) attrs;
+  # Simpler version of a `concatMapAttrs`: accepts only suffix as an argument and simply concats value and suffix.
+  suffixKeys = suffix: attrs: lib.attrsets.concatMapAttrs (name: value: {"${name}${suffix}" = value;}) attrs;
+
+  # Concat to path-like keys (string actually).
+  concatParentToPathKeys = prefix: attrs: lib.attrsets.concatMapAttrs (name: value: {"${prefix}/${name}" = value;}) attrs;
+  # Concat to path-like keys (string actually).
+  concatChildToPathKeys = suffix: attrs: lib.attrsets.concatMapAttrs (name: value: {"${name}/${suffix}" = value;}) attrs;
 
   # STYLE: Add get-path as an easier separate step. Or add it to `prefixValues`
   # and `prefixKeys`. Use it here only as a last step.
